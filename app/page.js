7c8246ffec41cc945621f22f7b3b9160c0da0aba
KavinduDissanayaka <ndkmadhuranga@std.appsc.sab.ac.lk>
2024-09-25 11:20:39 +0530

    update code

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
+          
           width={180}
           height={38}
           priority
