f6d7aab83c287278f8b6e88151830a4613b61ca1
KavinduDissanayaka <ndkmadhuranga@std.appsc.sab.ac.lk>
2024-10-09 18:19:07 +0530

    create delete blog feature

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -4,6 +4,7 @@ import BlogTableItem from '@/Components/AdminComponents/BlogTableItem'
 import axios from 'axios'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 
 
 
@@ -14,6 +15,16 @@ const [blogs,setBlogs] = useState([]);
 const fetchBlogs = async () =>{
   const response = await axios.get('/api/blog');
   setBlogs(response.data.blogs);
+}
+
+const deleteBlog = async (mongoId) =>{
+  const response = await axios.delete('/api/blog',{
+    params:{
+      id:mongoId
+    }
+  })
+  toast.success(response.data.msg);
+  fetchBlogs();
 }
 
 useEffect(()=>{
@@ -50,7 +61,7 @@ useEffect(()=>{
           </thead>
           <tbody>
             {blogs.map((item,index)=>{
-              return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date}/>
+              return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date} deleteBlog={deleteBlog}/>
             })}
               
           </tbody>
