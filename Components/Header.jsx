ae7e91f9f10ada911bd466be28ca08b0313c43b6
KavinduDissanayaka <ndkmadhuranga@std.appsc.sab.ac.lk>
2024-10-11 00:13:11 +0530

    complete Email subscription

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,8 +1,28 @@
 import { assets } from '@/Assets/assets'
+import axios from 'axios';
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+
 
 const Header = () => {
+
+const [email,setEmail] = useState("");
+
+const onSubmitHandler = async (e) =>{
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("email",email);
+    const response = await axios.post('/api/email',formData);
+    if (response.data.success){
+        toast.success(response.data.msg);
+        setEmail("");
+    }
+    else{
+        toast.error("Error")
+    }
+}
+
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
         <div className="flex justify-between items-center">
@@ -18,8 +38,8 @@ const Header = () => {
                      printing and typesetting industry.
                      Lorem ipsum has been the industry's standard dummy text ever.
                 </p>
-                <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action="">
-                    <input type="email" placeholder='Enter your email' className='pl-4 outline-none' />
+                <form onSubmit={onSubmitHandler} className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action="">
+                    <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder='Enter your email' className='pl-4 outline-none' />
                     <button type='submit' className='border-l border-black py-4 px-2 sm:px-8 active:bg-gray-600 active:text-white'>Subscribe</button>
                 </form>
             </div>
