484431ce5a6f06b316a3e0cc1a0ddc109aa2047b
KavinduDissanayaka <ndkmadhuranga@std.appsc.sab.ac.lk>
2024-10-13 22:01:14 +0530

    Complete Email subscription page for Admin

--- app/admin/subscriptions/page.jsx
+++ app/admin/subscriptions/page.jsx
@@ -1,11 +1,73 @@
 'use client'
-import React from 'react'
+import { assets } from '@/Assets/assets'
+import SubTableItem from '@/Components/AdminComponents/SubTableItem'
+import axios from 'axios'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 
 const page = () => {
-  return (
-    <div>
 
+  const [emails,setEmails] = useState([]);
+
+  const fetchEmails = async () => {
+    const response = await axios.get('/api/email');
+    setEmails(response.data.emails)
+  }
+
+  const deleteEmail = async (mongoId) =>{
+    const response = await axios.delete('/api/email',{
+      params:{
+        id:mongoId
+      }
+    })
+    if (response.data.success){
+      toast.success(response.data.msg);
+      fetchEmails();
+    }
+    else{
+      toast.error("Error");
+    }
+  }
+
+  useEffect(()=>{
+    fetchEmails();
+  },[])
+  return (
+    <>
+    <div className="flex flex-col w-full">
+          <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
+            <h3 className="font-medium">Admin Panel</h3>
+            <Image src={assets.profile_icon} width={40} alt="" />
+          </div> 
+          </div>
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+      <h1>All Subscription</h1>
+      <div className='relative max-w-[600px] h-[80vh] overflow-x-auto mt-4 border border-gray-400 scollbar-hide '>
+        <table className='w-full text-sm text-gray-500'>
+          <thead className='text-xs text-left text-gray-700 uppercase bg-gray-50'>
+            <tr>
+              <th scope='col' className='px-6 py-3'>
+                Email Subscription
+              </th>
+              <th scope='col' className=' hidden sm:block px-6 py-3'>
+                Date
+              </th>
+              <th scope='col' className='px-6 py-3'>
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {emails.map((item,index)=>{
+              return <SubTableItem key={index} mongoId={item._id} deleteEmail={deleteEmail} email={item.email} date={item.date} />
+            })}
+            
+          </tbody>
+        </table>
+      </div>
     </div>
+    </>
   )
 }
 
