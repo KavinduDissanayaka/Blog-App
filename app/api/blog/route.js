e3ffabc5a860e847f4c787ed6658e7e55de3a96a
KavinduDissanayaka <ndkmadhuranga@std.appsc.sab.ac.lk>
2024-10-13 13:10:28 +0530

    change code

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,6 +1,6 @@
 import { ConnectDB } from "@/lib/config/db"
 import BlogModel from "@/lib/models/BlogModel";
-const { NextResponse } = require("next/server")
+const { NextResponse } = require ("next/server")
 import {writeFile} from 'fs/promises'
 const fs = require('fs')
 
